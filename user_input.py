fe6b8482fdcf519b7c336239f9dd003ac8b4eb17
Didah Dassa <didahdassa@gmail.com>
2024-02-23 10:30:59 +0300

    Initial commit

---
+++ user_input.py
@@ -0,0 +1,20 @@
+name = input("What is your name? \n")
+age = input("How old are you? \n")
+location = input("Where are you living? \n")
+region = input("Which region? \n")
+country = input("Which country? \n")
+
+print("Hello {}, you are {} years old. Your living at {}, {} from {}".format(name, age, location, region, country))
+
+# OUTPUT
+# What is your name?
+# didah
+# How old are you?
+# 28
+# Where are you living?
+# sakina
+# Which region?
+# arusha
+# Which country?
+# tanzania
+# Hello didah, you are 28 years old. Your living at sakina, arusha from tanzania
